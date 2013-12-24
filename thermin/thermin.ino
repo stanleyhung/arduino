ced2d4b13417dc2b00bf3cb39cbaa952f68dc02b
stanleyhung <stanley.hung25@berkeley.edu>
2013-12-23 22:11:17 -0800

    added a simple piezo sound program

---
+++ thermin/thermin.ino
@@ -0,0 +1,30 @@
+int sensorValue;
+int sensorLow = 1023;
+int sensorHigh = 0;
+const int speakerPin = 8;
+const int ledPin = 1;
+const int switchPin = 2;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  
+  while (millis() < 5000) {
+    sensorValue = analogRead(A0);
+    if (sensorValue > sensorHigh) {
+      sensorHigh = sensorValue;
+    }
+    if (sensorValue < sensorLow) {
+      sensorLow = sensorValue;
+    }
+  }
+  
+  digitalWrite(ledPin, LOW);
+}
+
+void loop () {
+    sensorValue = analogRead(A0);
+    int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 2000);
+    tone(speakerPin, pitch, 20);
+    delay(10);
+}
