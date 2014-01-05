80d66130d9a84da43738d4dccdfce0d995bef2b7
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-04 17:07:44 -0800

    added print statements to SD card testing

--- sdtest/sdtest.ino
+++ sdtest/sdtest.ino
@@ -5,6 +5,7 @@ char *fileName;
 
 void setup() {
  SD.begin();
+ Serial.begin(9600);
  pinMode(ledPin, OUTPUT);
  fileName = (char*) malloc(sizeof(name));
  strcpy(fileName, name);
@@ -15,9 +16,11 @@ void setup() {
  */
 void loop() {
   if (SD.exists(fileName)) {
+    Serial.println("file exists in SD Card");
     digitalWrite(ledPin, HIGH);
     delay(1000);
     digitalWrite(ledPin, LOW);
     delay(1000);
   }
+  Serial.println("file does not exist in SD Card");
 }
