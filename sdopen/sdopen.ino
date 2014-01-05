88d533db4f75903852cf4de7ad67d4230bd5101d
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-04 17:13:36 -0800

    added sdopen to test open a file on SD Card

---
+++ sdopen/sdopen.ino
@@ -0,0 +1,16 @@
+#include <SD.h>
+
+void setup() {
+  char* fileName = "low.wav";
+  SD.begin();
+  Serial.begin(9600);
+  Serial.println(fileName);
+  if (SD.exists(fileName)) {
+    Serial.println("exists in SD Card");
+  } else {
+    Serial.println("does not exist in SD Card");
+  }
+}
+
+void loop () {
+}
