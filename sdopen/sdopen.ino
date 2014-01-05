da45ea3322c33700e629c35110daf1b9669979ec
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-04 17:23:27 -0800

    moved SD.begin() in sdopen

--- sdopen/sdopen.ino
+++ sdopen/sdopen.ino
@@ -1,13 +1,13 @@
 #include <SD.h>
 
 void setup() {
+  Serial.begin(9600);
   char* fileName = "low.wav";
   Serial.println("testing SD Card...");
   if (!SD.begin()) {
     Serial.println("ERROR - SD Card could not be opened");
     return;
   }
-  Serial.begin(9600);
   Serial.println(fileName);
   if (SD.exists(fileName)) {
     Serial.println("exists in SD Card");
