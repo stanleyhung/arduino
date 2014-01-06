eabe2fbdf8cfe81db219318914f9625210f9ec53
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-05 22:06:24 -0800

    moved WavParse library to correct location

--- dac/dac.ino
+++ dac/dac.ino
@@ -1,40 +1,9 @@
 #include <SD.h>
+#include <WavParse.h>
 
 File myFile;
 char* fileName = "low.wav";
-
-typedef union File_Header {
-  typedef struct Header {
-    unsigned long chunkID;
-    unsigned long chunkSize;
-    unsigned long format;
-  } Header;
-  Header h;
-  byte b[12];
-} File_Header;
-
-typedef union Wave_Header {
-  typedef struct Header {
-    unsigned long subchunkID;
-    unsigned long subchunksize;
-  } Header;
-  Header h;
-  byte b[8];
-} Wave_Header;
-
-typedef union Wave_Data {
-  typedef struct Data {
-    unsigned short audioFormat;
-    unsigned short numChannels;
-    unsigned long sampleRate;
-    unsigned long byteRate;
-    unsigned short blockAlign;
-    unsigned short bitsPerSample;
-    byte unused[8];
-  } Data;
-  Data d;
-  byte b[24];
-} Wave_Data;
+WavParse WavParse();
 
 void setup() {
   Serial.begin(9600);
@@ -45,7 +14,7 @@ void setup() {
     return;
   }
   Serial.print(fileName);
-  Serial.print("\t");
+  Serial.print()("\t");
   if (SD.exists(fileName)) {
     Serial.println("exists in SD Card");
   } else {
