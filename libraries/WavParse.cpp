2da8e540f104e4cb4d4c848e44873e5ff9ce0b46
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-05 21:52:23 -0800

    added skeleton code to .cpp file in WavParse library

---
+++ libraries/WavParse.cpp
@@ -0,0 +1,24 @@
+/*
+ * WavParse.cpp - Library for parsing header information from WAV files
+ * Created by Stanley Hung, January 1, 2014.
+ */
+
+#include "Arduino.h"
+#include "WavParse.h"
+
+WavParse::WavParse() {
+
+}
+
+int WavParse::checkFileHeader(*File_Header fh) {
+	return 1;
+}
+int WavParse::checkWaveHeader(*Wave_Header wh) {
+	return 1;
+}
+int WavParse::checkWaveData(*Wave_Data wd) {
+	return 1;
+}
+
+
+
