18de829394939746008c59ce7dd2bc8131909413
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-05 22:29:19 -0800

    implemented checkFileHeader in WavParse library

--- libraries/WavParse/WavParse.cpp
+++ libraries/WavParse/WavParse.cpp
@@ -6,12 +6,28 @@
 #include "Arduino.h"
 #include "WavParse.h"
 
+// The following fields map ASCII characters to little-endian decimal
+#define RIFF 1179011410
+#define WAVE 1163280727
+
 WavParse::WavParse() {
 	;
 }
 
+// This function ensures that a fileheader is correctly formatted.
 int WavParse::checkFileHeader(File_Header *fh) {
+	if (fh->h.chunkID != RIFF) {
+		return 0;
+	}
+	//Ensure that the chunkSize is positive
+	if (fh->h.chunkSize <= 0) {
+		return 0;
+	}
+	if (fh->h.format != WAVE) {
+		return 0;
+	}
 	return 1;
+	
 }
 int WavParse::checkWaveHeader(Wave_Header *wh) {
 	return 1;
