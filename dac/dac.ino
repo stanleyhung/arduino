7751c80f5ef9a78b32a393ab82a551dbd668607f
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-05 20:11:58 -0800

    added file open in setup()

--- dac/dac.ino
+++ dac/dac.ino
@@ -3,6 +3,12 @@
 File myFile;
 char* fileName = "low.wav";
 
+typedef struct header_file {
+  char chunkID[4];
+  char chunkSize[4];
+  char format[4];
+} header;
+
 void setup() {
   Serial.begin(9600);
   pinMode(10, OUTPUT);
@@ -11,11 +17,19 @@ void setup() {
     return;
   }
   Serial.print(fileName);
+  Serial.print("\t");
   if (SD.exists(fileName)) {
     Serial.println("exists in SD Card");
   } else {
     Serial.println("does not exist in SD Card");
   }
+  myFile = SD.open(fileName);
+  if (!myFile) {
+    Serial.println("Error - Could not open file");
+    return;
+  }
+  Serial.println("successfully opened file");
+  myFile.close();
 }
 
 void loop () {
