1bd18220c524140ccc483bc0b841344b131e208e
stanleyhung <stanley.hung25@berkeley.edu>
2014-01-05 21:47:04 -0800

    added functions to wavParse library header file

--- libraries/WavParse.h
+++ libraries/WavParse.h
@@ -10,44 +10,41 @@
 class WavParse {
 public:
 	WavParse();
-	typedef struct File_Header {
-	  union {
-	    typedef struct Header {
-	      unsigned long chunkID;
-	      unsigned long chunkSize;
-	      unsigned long format;
-	    } Header;
-	    Header h;
-	    byte b[12];
-	  } payLoad;
+	typedef union File_Header {
+	  typedef struct Header {
+	    unsigned long chunkID;
+	    unsigned long chunkSize;
+	    unsigned long format;
+	  } Header;
+	  Header h;
+	  byte b[12];
 	} File_Header;
 
-	typedef struct Wave_Header {
-	  union {
-	    typedef struct Header {
-	      unsigned long subchunkID;
-	      unsigned long subchunksize;
-	    } Header;
-	    Header h;
-	    byte b[8];
-	  } payLoad;
+	typedef union Wave_Header {
+	  typedef struct Header {
+	    unsigned long subchunkID;
+	    unsigned long subchunksize;
+	  } Header;
+	  Header h;
+	  byte b[8];
 	} Wave_Header;
 
-	typedef struct Wave_Data {
-	  union {
-	    typedef struct Data {
-	      unsigned short audioFormat;
-	      unsigned short numChannels;
-	      unsigned long sampleRate;
-	      unsigned long byteRate;
-	      unsigned short blockAlign;
-	      unsigned short bitsPerSample;
-	      byte unused[8];
-	    } Data;
-	    Data d;
-	    byte b[24];
-	  } payLoad;
+	typedef union Wave_Data {
+	  typedef struct Data {
+	    unsigned short audioFormat;
+	    unsigned short numChannels;
+	    unsigned long sampleRate;
+	    unsigned long byteRate;
+	    unsigned short blockAlign;
+	    unsigned short bitsPerSample;
+	    byte unused[8];
+	  } Data;
+	  Data d;
+	  byte b[24];
 	} Wave_Data;
+	int checkFileHeader(*File_Header fh);
+	int checkWaveHeader(*Wave_Header wh);
+	int checkWaveData(*Wave_Data wd);
 
 }
 
